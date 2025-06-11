ed55c0f4f64a87cd166e9df9a8f82e5067a7ea09
Nimira43 <n.rabone72@aol.com>
2025-06-11 13:55:16 +0100

    Fire class done. Copied code to Water class.

--- main.py
+++ main.py
@@ -73,13 +73,63 @@ class Fire(Synkrusis):
     print('It dealt ' + str(damage) + ' damage.')
     enemy.current_health -= damage
   
-  def move_info():
-    pass
+  def move_info(self):
+    print('\n' + self.name + 'Moves: ')
+    print('.. ' + self.moves[0] + ' ..')
+    print('\t an efficient attack...')
+    print('\tguaranteed to do damage with 15 to 25 points.')
+    
+    print('.. ' + self.moves[1] + ' ..')
+    print('\t a risky attack...')
+    print('\tcould deal damage up to 50 points.')
+    
+    print('.. ' + self.moves[2] + ' ..')
+    print('\t a healing move...')
+    print('\tguaranteed to heal between 15 to 25 points.')
+    
+    print('.. ' + self.moves[3] + ' ..')
+    print('\t a fire based attack...')
+    print('\tguaranteed to do damage to Grass type Agent.')
 
 
 class Water():
-  pass
+  def __init__(self, name, element, health, speed):
+    super().__init__(self, name, element, health, speed)
+    moves = ['Scratch', 'Ember', 'Light', 'Fire Blast']
 
+
+  def special_attack(self, enemy):
+    print('Agent ' + self.name + ' used ' + self.moves[3])
+    
+    if enemy.element == 'Grass':
+      print('That was very effective.')
+      damage = random.randint(35, 50)
+    elif enemy.element == 'Water':
+      print('That was very ineffective.')
+      damage = random.randint(-5, 10)
+    else:
+      random.randint(10, 30)
+
+    print('It dealt ' + str(damage) + ' damage.')
+    enemy.current_health -= damage
+  
+  def move_info(self):
+    print('\n' + self.name + 'Moves: ')
+    print('.. ' + self.moves[0] + ' ..')
+    print('\t an efficient attack...')
+    print('\tguaranteed to do damage with 15 to 25 points.')
+    
+    print('.. ' + self.moves[1] + ' ..')
+    print('\t a risky attack...')
+    print('\tcould deal damage up to 50 points.')
+    
+    print('.. ' + self.moves[2] + ' ..')
+    print('\t a healing move...')
+    print('\tguaranteed to heal between 15 to 25 points.')
+    
+    print('.. ' + self.moves[3] + ' ..')
+    print('\t a fire based attack...')
+    print('\tguaranteed to do damage to Grass type Agent.')
 
 class Grass():
   pass
